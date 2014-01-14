6451551144c28d85fe8daef15f9b731523d6429b
Denis Papathanasiou <dp2704@columbia.edu>
2014-01-14 15:27:42 -0500

    Fixed a problem reading the reply from the server: ioutil.ReadAll() will never return b/c it will never get an EOF from the modbus slave

--- modbus-core.go
+++ modbus-core.go
@@ -5,30 +5,32 @@
 package modbusclient
 
 const (
-    MODBUS_PORT = 502
+	MODBUS_PORT       = 502
+	RTU_FRAME_MAXSIZE = 512
+	TCP_FRAME_MAXSIZE = 260
 
-    FUNCTION_READ_COILS                    = 0x01
-    FUNCTION_READ_DISCRETE_INPUTS          = 0x02
-    FUNCTION_READ_HOLDING_REGISTERS        = 0x03
-    FUNCTION_READ_INPUT_REGISTERS          = 0x04
-    FUNCTION_WRITE_SINGLE_COIL             = 0x05
-    FUNCTION_WRITE_SINGLE_REGISTER         = 0x06
-    FUNCTION_WRITE_MULTIPLE_REGISTERS      = 0x10
-    FUNCTION_MODBUS_ENCAPSULATED_INTERFACE = 0x2B
+	FUNCTION_READ_COILS                    = 0x01
+	FUNCTION_READ_DISCRETE_INPUTS          = 0x02
+	FUNCTION_READ_HOLDING_REGISTERS        = 0x03
+	FUNCTION_READ_INPUT_REGISTERS          = 0x04
+	FUNCTION_WRITE_SINGLE_COIL             = 0x05
+	FUNCTION_WRITE_SINGLE_REGISTER         = 0x06
+	FUNCTION_WRITE_MULTIPLE_REGISTERS      = 0x10
+	FUNCTION_MODBUS_ENCAPSULATED_INTERFACE = 0x2B
 
-    EXCEPTION_ILLEGAL_FUNCTION                        = 0x01
-    EXCEPTION_DATA_ADDRESS                            = 0x02
-    EXCEPTION_DATA_VALUE                              = 0x03
-    EXCEPTION_SLAVE_DEVICE_FAILURE                    = 0x04
-    EXCEPTION_ACKNOWLEDGE                             = 0x05
-    EXCEPTION_SLAVE_DEVICE_BUSY                       = 0x06
-    EXCEPTION_MEMORY_PARITY_ERROR                     = 0x08
-    EXCEPTION_GATEWAY_PATH_UNAVAILABLE                = 0x0A
-    EXCEPTION_GATEWAY_TARGET_DEVICE_FAILED_TO_RESPOND = 0x0B
+	EXCEPTION_ILLEGAL_FUNCTION                        = 0x01
+	EXCEPTION_DATA_ADDRESS                            = 0x02
+	EXCEPTION_DATA_VALUE                              = 0x03
+	EXCEPTION_SLAVE_DEVICE_FAILURE                    = 0x04
+	EXCEPTION_ACKNOWLEDGE                             = 0x05
+	EXCEPTION_SLAVE_DEVICE_BUSY                       = 0x06
+	EXCEPTION_MEMORY_PARITY_ERROR                     = 0x08
+	EXCEPTION_GATEWAY_PATH_UNAVAILABLE                = 0x0A
+	EXCEPTION_GATEWAY_TARGET_DEVICE_FAILED_TO_RESPOND = 0x0B
 )
 
 type TCPFrame struct {
-    TransactionID int
-    FunctionCode  byte
-    Data          []byte
+	TransactionID int
+	FunctionCode  byte
+	Data          []byte
 }
--- modbus-tcp.go
+++ modbus-tcp.go
@@ -5,50 +5,57 @@
 package modbusclient
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net"
+	"fmt"
+	"net"
 )
 
 // GenerateTCPFrame is a method corresponding to a TCPFrame object which
 // returns a byte array representing the associated TCP/IP application data
 // unit (ADU)
 func (frame *TCPFrame) GenerateTCPFrame() []byte {
-    packetLen := len(frame.Data) + 8             // 7 bytes for the header + 1 for the function code
-    packet := make([]byte, packetLen)            //len(m.Data)+8) // 7 bytes for the header + 1 for the function code
-    packet[0] = byte(frame.TransactionID >> 8)   // Transaction ID (High Byte)
-    packet[1] = byte(frame.TransactionID & 0xff) //                (Low Byte)
-    packet[2] = 0x00                             // Protocol ID (2 bytes) -- always 00
-    packet[3] = 0x00
-    packet[4] = byte((packetLen - 4) >> 8)   // Remaining length of packet (High Byte)
-    packet[5] = byte((packetLen - 4) & 0xff) //                            (Low Byte)
-    packet[6] = 0x01                         // Unit ID (1 byte)
-    packet[7] = frame.FunctionCode
-    packet = append(packet, frame.Data...)
-    fmt.Println(fmt.Sprintf("%x", packet))
+	packetLen := len(frame.Data) + 8             // 7 bytes for the header + 1 for the function code
+	packet := make([]byte, packetLen)            //len(m.Data)+8) // 7 bytes for the header + 1 for the function code
+	packet[0] = byte(frame.TransactionID >> 8)   // Transaction ID (High Byte)
+	packet[1] = byte(frame.TransactionID & 0xff) //                (Low Byte)
+	packet[2] = 0x00                             // Protocol ID (2 bytes) -- always 00
+	packet[3] = 0x00
+	packet[4] = byte((packetLen - 4) >> 8)   // Remaining length of packet (High Byte)
+	packet[5] = byte((packetLen - 4) & 0xff) //                            (Low Byte)
+	packet[6] = 0x01                         // Unit ID (1 byte)
+	packet[7] = frame.FunctionCode
+	packet = append(packet, frame.Data...)
 
-    return packet
+	return packet
 }
 
 // TransmitAndReceive is a method corresponding to a TCPFrame object which
 // generates the corresponding ADU, transmits it to the modbus server
-// (slave device) specified by the TCP address, and returns a byte array
+// (slave device) specified by the TCP address+port, and returns a byte array
 // of the slave device's reply, and error (if any)
-func (frame *TCPFrame) TransmitAndReceive(slaveAddress string) ([]byte, error) {
-    adu := frame.GenerateTCPFrame()
-    addr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf("%s:%d", slaveAddress, MODBUS_PORT))
-    if err == nil {
-        conn, err := net.DialTCP("tcp", nil, addr)
-        defer conn.Close()
-        if err == nil {
-            _, err = conn.Write(adu)
-            if err == nil {
-                res, err := ioutil.ReadAll(conn)
-                if err == nil {
-                    return res, nil
-                }
-            }
-        }
-    }
-    return []byte{}, err
+func (frame *TCPFrame) TransmitAndReceive(server string, port int) ([]byte, error) {
+	adu := frame.GenerateTCPFrame() // generate the ADU
+	addr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf("%s:%d", server, port))
+
+	if err == nil {
+		// attempt to connect to the slave device (server)
+		conn, err := net.DialTCP("tcp", nil, addr)
+		defer conn.Close()
+
+		if err == nil {
+			// transmit the ADU
+			_, err = conn.Write(adu)
+
+			if err == nil {
+				// read the response
+				response := make([]byte, TCP_FRAME_MAXSIZE)
+				n, err := conn.Read(response)
+
+				if err == nil {
+					// return only the number of bytes read
+					return response[:n], nil
+				}
+			}
+		}
+	}
+	return []byte{}, err
 }
